a5aeb854ead533ed38185dcafa9e9105c20c02f5
spider-hand <creative.spider.hand@gmail.com>
2026-03-05 00:32:40 +0900

    feat: add entry

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -8,12 +9,25 @@ import (
 )
 
 type Entry struct {
-	Key string
-	Cmd string
+	Key string `json:"key"`
+	Cmd string `json:"cmd"`
+}
+
+func getDataFilePath() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "entries.json"
+	}
+
+	dir := home + "/.recall"
+
+	os.MkdirAll(dir, 0755)
+
+	return dir + "/entries.json"
 }
 
 func loadEntries() []Entry {
-	file, err := os.ReadFile("entries.json")
+	file, err := os.ReadFile(getDataFilePath())
 	if err != nil {
 		return []Entry{}
 	}
@@ -38,11 +52,46 @@ func search(entries []Entry, query string) []Entry {
 	return results
 }
 
+func addEntry() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("description: ")
+	key, _ := reader.ReadString('\n')
+	key = strings.TrimSpace(key)
+
+	fmt.Print("command: ")
+	cmd, _ := reader.ReadString('\n')
+	cmd = strings.TrimSpace(cmd)
+
+	entries := loadEntries()
+
+	entry := Entry{Key: key, Cmd: cmd}
+
+	entries = append(entries, entry)
+
+	saveEntries(entries)
+}
+
+func saveEntries(entries []Entry) {
+	data, err := json.MarshalIndent(entries, "", "  ")
+	if err != nil {
+		return
+	}
+
+	os.WriteFile(getDataFilePath(), data, 0644)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("usage: recall <query>")
 		return
 	}
+
+	if os.Args[1] == "add" {
+		addEntry()
+		return
+	}
+
 	query := strings.Join(os.Args[1:], " ")
 
 	entries := loadEntries()
